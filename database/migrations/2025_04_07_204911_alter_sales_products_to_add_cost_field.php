2f78ad8e99329df2ece68984a23f5b2914c9f958
Jackson Echeverri <jackson@lesiga.com>
2025-04-07 23:07:21 +0200

    UPD: Ajuste en migración

--- database/migrations/2025_04_07_204911_alter_sales_products_to_add_cost_field.php
+++ database/migrations/2025_04_07_204911_alter_sales_products_to_add_cost_field.php
@@ -17,15 +17,13 @@ class AlterSalesProductsToAddCostField extends Migration
     public function up()
     {
         Schema::table('sales_products', function (Blueprint $table) {
-            Schema::table('sales_products', function (Blueprint $table) {
-                $table->double('cost')->default(0)->after('quantity');
-            });
-    
-            // Update the product cost in all sales with the current product value based on product id
-            SaleProduct::query()->update([
-                'cost' => Product::query()->where('id', SaleProduct::query()->value('product_id'))->value('cost') ?? 0
-            ]);
+            $table->double('cost')->default(0)->after('quantity');
         });
+
+        // Update the product cost in all sales with the current product value based on product id
+        SaleProduct::query()->update([
+            'cost' => Product::query()->where('id', SaleProduct::query()->value('product_id'))->value('cost') ?? 0
+        ]);
     }
 
     /**
